86d8313c3f5ea49c4148214b22906475e161d4fe
Anderson Mendes <anderson2000ano@gmail.com>
2018-11-24 19:08:21 -0200

    dope af :V

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 	if(localStorage.getItem('accent') != null){
 		var color = localStorage.getItem('accent')
+		handlerBg(localStorage.getItem('bg'))
 		document.getElementsByTagName("body")[0].style.setProperty("--accent",color)
 		$("meta[name='theme-color']").attr("content", color);
 		$("meta[name='msapplication-TileColor']").attr("content", color);
@@ -17,5 +18,24 @@ function setColor(obj) {
  }
 
 function setBg(obj) {
+	var color = $(obj).attr("data-hex");
+	localStorage.setItem('bg',color)
+	handlerBg(color);
 
+}
+
+function handlerBg(hex){
+	if(hex == "#212121"){
+		document.getElementsByTagName("body")[0].style.setProperty("--background",hex)
+		document.getElementsByTagName("body")[0].style.setProperty("--card","#2d2d2d")
+		document.getElementsByTagName("body")[0].style.setProperty("--text","white")
+	}else if (hex == "#000"){
+		document.getElementsByTagName("body")[0].style.setProperty("--background",hex)
+		document.getElementsByTagName("body")[0].style.setProperty("--card","#0f0f0f")
+		document.getElementsByTagName("body")[0].style.setProperty("--text","#9D9D9D")
+	}else{
+		document.getElementsByTagName("body")[0].style.setProperty("--background",hex)
+		document.getElementsByTagName("body")[0].style.setProperty("--card","white")
+		document.getElementsByTagName("body")[0].style.setProperty("--text","black")
+	}
 }
