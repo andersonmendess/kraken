a82c0180a1ea381d7ad00167dc045f1db8bbf169
Anderson Mendes <anderson2000ano@gmail.com>
2019-06-08 22:09:15 -0300

    sitemap: cleanup

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -7,7 +7,7 @@ baseSITE = "https://krakenproject.github.io/"
 date = datetime.now().strftime("%Y-%m-%d")
 
 xml = '<?xml version="1.0" encoding="UTF-8"?>'
-xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'
+xml += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 xml += "\n<url><loc>" + baseSITE + "</loc><lastmod>"+date+"</lastmod><priority>1.00</priority></url>"
 
 request = urllib.request.urlopen(devicesJSON)
@@ -18,7 +18,7 @@ for device in devices:
 
 xml += "\n</urlset>"
 
-
 file = open('sitemap.xml', 'w')
 file.write(xml)
+
 print("SITEMAP GERENATED -> sitemap.xml")
