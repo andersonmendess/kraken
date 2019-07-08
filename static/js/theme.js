6f7f064b32d38d0e6dcacf5ceb37195e4cd05f56
Anderson Mendes <anderson2000ano@gmail.com>
2019-07-08 13:54:22 -0300

    update imgs and default accent

--- static/js/theme.js
+++ static/js/theme.js
@@ -9,7 +9,7 @@ window.onload = function () {
 const colors = {
 	"#ffffff": ['white', '#161616'], // card, text for white
 	"#000000": ['#090909', '#f7f7f7'], // card, text for black
-	"default": ['#1C1C1C', '#f7f7f7', '#a97cf0', '#212121'] // card, text, accent, bg for default
+	"default": ['#1C1C1C', '#f7f7f7', '#5485d8', '#212121'] // card, text, accent, bg for default
 }
 
 const toogleMenu = () => {
